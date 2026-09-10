96fc00bafb7038e9cf91eed4ac9ab5f4fbad08e1
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-10 22:51:27 +0530

    Load centralized EventHive theme globally

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './theme.css';
 import App from './App';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
