ade143a10ee0ae21363c98dbb0fc783dbb103af9
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-12 22:18:34 +0530

    Place mobile app download section at homepage bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import useGetEvent from "../AdminCustomHooks/useGetEvents";
 import HomeHero from "./home/HomeHero";
 import HomeSections from "./home/HomeSections";
+import AppDownloadSection from "./home/AppDownloadSection";
 
 const Home = () => {
   const { isLoading, data, error } = useGetEvent();
@@ -27,6 +28,7 @@ const Home = () => {
         isLoading={isLoading}
         error={error}
       />
+      <AppDownloadSection featuredEvent={featuredEvents[0]} />
     </div>
   );
 };
